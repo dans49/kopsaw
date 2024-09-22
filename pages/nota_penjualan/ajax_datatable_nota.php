a30bce8b9126a302c05ed6980b367c18f5f92720
mridwann19 <mrid423@gmail.com>
2024-09-22 20:04:01 +0700

    On branch dev Your branch is ahead of 'origin/dev' by 2 commits.   (use "git push" to publish your local commits)

--- pages/nota_penjualan/ajax_datatable_nota.php
+++ pages/nota_penjualan/ajax_datatable_nota.php
@@ -158,9 +158,9 @@ if ($_GET['action'] == "table_data") {
             $nestedData['status'] = $status;
             $nestedData['pic'] = $r['pic'];
             $nestedData['kasir'] = $r['nama'];
-            $nestedData['aksi'] = "<button href='#'' class='btn btn-primary btn-sm det_nota' data-idnota= '".$r['id_nota']."' data-toggle='modal' data-target='#edit_nota'";
+            $nestedData['aksi'] = "<button href='#'' class='btn btn-primary btn-sm det_nota' data-idnota= '" . $r['id_nota'] . "' data-toggle='modal' data-target='#edit_nota'";
             $nestedData['aksi'] .= "<span class='icon text-white'><i class='fas fa-search'></i></span></button> ";
-            $nestedData['aksi'] .= "<button class='btn btn-danger btn-sm hapus_nota' data-toggle='modal' data-idnh='".$r['id_nota']."' data-target='#hapus_nota'>";
+            $nestedData['aksi'] .= "<button class='btn btn-danger btn-sm hapus_nota' data-toggle='modal' data-idnh='" . $r['id_nota'] . "' data-target='#hapus_nota'>";
             $nestedData['aksi'] .= "<span class='icon text-white'><i class='fas fa-trash'></i></span></button>";
 
 
